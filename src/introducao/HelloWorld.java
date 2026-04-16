59842415df12195e406529c0777a1d78a096797d
Raissa <raissa.feitosa06@gmail.com>
2026-04-16 17:00:36 -0300

    Inicializando meu projeto aula 16/04

---
+++ src/introducao/HelloWorld.java
@@ -0,0 +1,11 @@
+package introducao;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {	// método principal que executa o código
+		
+		System.out.print("Hello World"); // idêntico ao escreva no portugol
+		
+	}
+
+}
---
+++ src/introducao/VariaveisConstantes.java
@@ -0,0 +1,25 @@
+package introducao;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		
+		// Variaveis
+		int idade = 25;
+		String nome = "Lilian";
+		float graus = 20.0f; 
+		boolean resposta;
+		
+		
+		//Constantes
+		final int QUANTIDADE = 50;
+		final String TURMA = "Turma 84";
+		final double AREA = 3.1415;
+		
+		System.out.print(nome); // não pula linha
+		System.out.println(QUANTIDADE);
+		System.out.println(graus);
+
+	}
+
+}
