f0d960323273de6b3ef5ffac5ffe1c68ecde4cff
Raissa <raissa.feitosa06@gmail.com>
2026-04-28 15:42:45 -0300

    Aula métodos 28/04

---
+++ src/metodoExterno/ExemploMetodoExterno.java
@@ -0,0 +1,14 @@
+package metodoExterno;
+
+public class ExemploMetodoExterno {
+
+	public static void main(String[] args) {
+		
+		info();
+
+	}
+	public static void info () {
+		System.out.println("Hello World");
+	}
+
+}
