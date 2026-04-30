fda93d9801df7b45501bf2e0bc8e9949003defb6
Raissa <raissa.feitosa06@gmail.com>
2026-04-30 10:13:48 -0300

    Aula POO - Herança 30/04

---
+++ src/pooHeranca/Estudante.java
@@ -0,0 +1,28 @@
+package pooHeranca;
+
+public class Estudante extends Pessoa{
+	
+	
+    private String matricula;
+    private String curso;
+    
+	public Estudante(String nome, int idade, String email, String matricula, String curso) {
+		super(nome, idade, email);
+		this.matricula = matricula;
+		this.curso = curso;
+	}
+	
+	
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+}
